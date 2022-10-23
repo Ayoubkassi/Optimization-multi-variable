b05abc2480b764d15a0b9468d20fe38c428f99b3
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-10-23 15:15:11 +0100

    add another ex

--- Exercices_TP/ex2.py
+++ Exercices_TP/ex2.py
@@ -1,4 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import scipy as sp
 
+import numpy as np
+from scipy import optimize
 
 data = [
 	[0,10,20,30,30,20],
@@ -9,10 +15,90 @@ data = [
 	[20,10,20,25,14,0]
 ]
 
-n = len(data)
+alpha = 15
 
-def objectif_func(x):
-	return np.sum(x)
+convertedData = []
 
-bnds = optimize.Bounds(0, 1)
+for i in range(len(data)):
+	current = []
+	for j in range(len(data)):
+		if data[i][j] <= alpha :
+			current.append(1) 
+		else : 
+			current.append(0)
+	convertedData.append(current)
+
+#print for test
+for row in convertedData:
+	print(row)
+
+sizes = np.array(convertedData)
+values = np.array([1,1,1,1,1,1])
+
+
+
+bounds = optimize.Bounds(0, 1)  # 0 <= x_i <= 1
+integrality = np.full_like(values, True)  # x_i are integers
+
+def calcSumForI(i):
+	curr = []
+	for j in range(len(convertedData)) :
+		res = 0
+		res+=convertedData[i][j]*values[j]
+		curr.append(res)
+	return curr
+
+def giveArray(k):
+	tab = np.zeros(6)
+	tab[0] = k
+	return tab
+
+def getArr(k):
+	tab = []
+	for i in range(len(values)):
+	    if i != k :
+		    tab.append(0)
+	    else :
+		    tab.append(values[i])
+	return tab
+
+cond1 = calcSumForI(0)
+cond2 = calcSumForI(1)
+cond3 = calcSumForI(2)
+cond4 = calcSumForI(3)
+cond5 = calcSumForI(4)
+cond6 = calcSumForI(5)
+cond7 = getArr(0)
+cond_pre_8 = np.add(getArr(0),getArr(1))
+cond8   = np.add(cond_pre_8,getArr(2))
+arr4 = getArr(4)
+arr5 = getArr(5)
+cond9 = np.add(arr4,arr5)
+
+
+
+A  = np.array([cond1,cond2,cond3,cond4,cond5,cond6,cond7,cond8,cond9])
+lb = np.array([1,1,1,1,1,1,0,1,1])
+ub = np.array([np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,values[3],3,1])
+
+
+
+cons = optimize.LinearConstraint(A,lb,ub)
+
+from scipy.optimize import milp
+res = milp(c=values, constraints=cons,integrality=integrality, bounds=bounds)
+res.success
+res.x
+
+print(res)
+
+
+
+# n = len(data)
+
+# def objectif_func(x):
+# 	return np.sum(x)
+
+# bnds = optimize.Bounds(0, 1)
+
 
--- Exercices_TP/ex3.py
+++ Exercices_TP/ex3.py
@@ -2,53 +2,55 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import scipy as sp
+
+import numpy as np
 from scipy import optimize
+
+
+
+
+sizes = np.array([1, 1, 1, 1, 1, 1])
+values = np.array([1.4,2.7,2.4,2.9,3.5,2])
+
+bounds = optimize.Bounds(0, 1)  # 0 <= x_i <= 1
+integrality = np.full_like(values, True)  # x_i are integers
+
+
+#define second sizes
+
+def getArr(k):
+	tab = []
+	for i in range(len(sizes)):
+	    if i != k :
+		    tab.append(0)
+	    else :
+		    tab.append(sizes[i])
+	return tab
+
+# def get2Arr(k,p):
+# 	tab = []
+# 	for i in range(len(sizes)):
+# 	    if i != k or i != p:
+# 		    tab.append(0)
+# 	    else :
+# 		    tab.append(sizes[i])
+# 	return tab
+
+sizes0 = getArr(0)
+sizes3 = getArr(3)
+sizes5 = getArr(5)
+sizes0_5 = np.add(sizes0,sizes5)
+
+A = np.array([sizes,sizes0,sizes3,sizes3,sizes0_5])
+lb= np.array([4,0,sizes[4],1-sizes[1]-sizes[4],1])
+ub= np.array([4,1-sizes[3],1,1,1])
+
+cons = optimize.LinearConstraint(A,lb,ub)
+
+
 from scipy.optimize import milp
-from scipy.optimize import minimize
+res = milp(c=values, constraints=cons,integrality=integrality, bounds=bounds)
+res.success
+res.x
 
-
-data = [1.4,2.7,2.4,2.9,3.5,2]
-n = len(data)
-
-def objectif_func(x):
-	sum =0
-	for i in range(n):
-		sum+=data[i]*x[i]
-	return sum 
-
-def constraint_1(x):
-	return np.sum(x) - 4 
-
-def constraint_2(x):
-	return  -x[0] - x[3] + 1
-
-def constraint_3(x):
-	return x[3] - x[4]
-
-def constraint_4(x):
-	return x[1] + x[3] + x[4] - 1
-
-def constraint_5(x):
-	return x[0] + x[5] - 1
-
-
-
-
-
-x0 = np.ones(n)
-bnds = optimize.Bounds(0, 1)
-integrality = np.full_like(x0, True) 
-
-constraint1 = { 'type' :'eq'     , 'fun' : constraint_1 }
-constraint2 = { 'type' : 'ineq'  , 'fun' : constraint_2 }
-constraint3 = { 'type' : 'ineq'  , 'fun' : constraint_3 }
-constraint4 = { 'type' : 'ineq'  , 'fun' : constraint_4 }
-constraint5 = { 'type' : 'eq'    , 'fun' : constraint_5 }
-cons = [constraint1,constraint2,constraint3,constraint4,constraint5]
-
-
-
-
-result = minimize(objectif_func,x0,method='SLSQP',bounds=bnds, constraints=cons,options={'ftol':1e-00})
-
-print(result)
+print(res)
